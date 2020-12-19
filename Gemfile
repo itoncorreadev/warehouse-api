a0548f5b675f27b813b2350d22a21d75a98cc5ba
Cleiton Correa <cleitoncorreas@gmail.com>
2020-12-19 12:56:02 -0300

    Adding devise user model

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'puma', '~> 3.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+# gem 'bcrypt', '~> 3.1.11'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -47,3 +47,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
-    namespace :v1, path: "/", constraints: AppVersionConstraint.new(version: 1, default: true) do
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
     end
   end
 end
