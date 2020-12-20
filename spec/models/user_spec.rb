a0f24a7e87c970d600120678cacbea1889f1682a
Cleiton Correa <cleitoncorreas@gmail.com>
2020-12-20 17:10:00 -0300

    Improving rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,5 @@ RSpec.describe User, type: :model do
   it { expect(@user).to respond_to(:name) }
   it { expect(@user).to respond_to(:password) }
   it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid  }
+  it { expect(@user).to be_valid }
 end
