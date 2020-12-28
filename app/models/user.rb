9429a126d0700d5ff9adac4cad8af030b0aa7c82
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2020-12-28 09:44:22 -0200

    Adding user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,11 @@ class User < ApplicationRecord
  def info
   "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
  end
- 
+
+ def generate_authentication_token!
+   begin
+     self.auth_token = Devise.friendly_token
+   end while User.exists?(auth_token: auth_token)
+ end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
 
-  it { is_expected.to validate_presence_of(:email) } 
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive } 
-  it { is_expected.to validate_confirmation_of(:password) } 
-  it { is_expected.to allow_value('cleitoncorreas@gmail.com').for(:email) } 
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value('cleitoncorreas@gmail.com').for(:email) }
   it { is_expected.to validate_uniqueness_of(:auth_token) }
 
   describe '#info' do
@@ -18,4 +18,22 @@ RSpec.describe User, type: :model do
       expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123xyzTOKEN")
     end
   end
+
+  describe '#generate_authentication_token!' do
+    it 'generates a unique auth token' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+      user.generate_authentication_token!
+
+      expect(user.auth_token).to eq('abc123xyzTOKEN')
+    end
+
+    it 'generates another auth token when the current auth token already has been token' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz', 'abc123tokenxyz', 'abcXYZ123456789')
+      existing_user = create(:user)
+      user.generate_authentication_token!
+
+      expect(user.auth_token).not_to eq(existing_user.auth_token)
+    end
+  end
+
 end
