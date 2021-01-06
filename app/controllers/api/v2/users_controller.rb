01367b353add5048a25c94ddfa6e3d4478aff8e1
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-06 18:54:25 -0200

    Implementinting JSONAPI.ORG patterns for users

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -1,11 +1,10 @@
 class Api::V2::UsersController < ApplicationController
   before_action :authenticate_with_token!, only: [:update, :destroy]
-  respond_to :json
 
   def show
     begin
-      @user = User.find(params[:id])
-      respond_with @user
+      user = User.find(params[:id])
+      render json: user, status: 200
     rescue
       head 404
     end
@@ -13,6 +12,7 @@ class Api::V2::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+
     if user.save
       render json: user, status: 201
     else
--- spec/requests/api/v2/users_spec.rb
+++ spec/requests/api/v2/users_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'Users API', type: :request do
 
     context 'when the user exists' do
       it 'returns the user' do
-        expect(json_body[:id]).to eq(user_id)
+        expect(json_body[:data][:id].to_i).to eq(user_id)
       end
 
       it 'returns status code 200' do
@@ -48,7 +48,7 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'retuns json data for the created user' do
-        expect(json_body[:email]).to eq(user_params[:email])
+        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
       end
     end
 
@@ -78,7 +78,7 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns the json data for the updated user' do
-        expect(json_body[:email]).to eq(user_params[:email])
+        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
       end
     end
 
