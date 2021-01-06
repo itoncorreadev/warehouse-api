ef0deac04d2c15de899f51d2365be9d284f4f30d
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-06 10:26:40 -0200

    Adding show action to the tasks controller with tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::TasksController < ApplicationController
     tasks = current_user.tasks
     render json: { tasks: tasks }, status: 200
   end
+
+  def show
+    task = current_user.tasks.find(params[:id])
+    render json: task, status: 200
+  end
 end
--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -26,4 +26,18 @@ RSpec.describe 'Task API' do
       expect(json_body[:tasks].count).to eq(5)
     end
   end
+
+  describe 'GET /tasks/:id' do
+    let(:task) { create(:task, user_id: user.id)}
+
+    before { get "/tasks/#{task.id}", params: {}, headers: headers }
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns the json for task' do
+      expect(json_body[:title]).to eq(task.title)
+    end
+  end
 end
