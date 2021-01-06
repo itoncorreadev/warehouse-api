288c0560e89fc536ae736e10e0c1d54244c98ec9
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-06 11:22:28 -0200

    Adding update action to the tasks controller with tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,18 +1,31 @@
 class Api::V1::TasksController < ApplicationController
   def index
     tasks = current_user.tasks
+
     render json: { tasks: tasks }, status: 200
   end
 
   def show
     task = current_user.tasks.find(params[:id])
+
     render json: task, status: 200
   end
 
   def create
     task = current_user.tasks.build(task_params)
+
     if task.save
       render json: task, status: 201
+    else
+      render json: { errors: task.errors }, status: 422
+    end
+  end
+
+  def update
+    task = current_user.tasks.find(params[:id])
+
+    if task.update_attributes(task_params)
+      render json: task, status: 200
     else
       render json: { errors: task.errors }, status: 422
     end
