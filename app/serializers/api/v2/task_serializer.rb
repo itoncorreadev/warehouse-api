f791feb498064e2f8c54c0f35455f14c8b4de419
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-07 20:47:12 -0200

    Fixing task_serializer

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -1,11 +1,11 @@
 class Api::V2::TaskSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :done, :deadline, :user_id, :created_at, :updated_at, :short_description, :is_late?
+  attributes :id, :title, :description, :done, :deadline, :user_id, :created_at, :updated_at, :short_description, :is_late
 
   def short_description
-    object.description[0..40]
+    object.description[0..40] if object.description.present?
   end
 
-  def is_late?
+  def is_late
     Time.current > object.deadline if object.deadline.present?
   end
 end
