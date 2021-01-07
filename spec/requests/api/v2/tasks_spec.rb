453823548c1308c5cb58a555a0e0944f91cd034e
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-07 18:57:01 -0200

    Updating the Tasks API

--- spec/requests/api/v2/tasks_spec.rb
+++ spec/requests/api/v2/tasks_spec.rb
@@ -4,11 +4,15 @@ RSpec.describe 'Task API' do
   before { host! 'api.warehouse.test' }
 
   let!(:user) { create(:user) }
+  let!(:auth_data) {  user.create_new_auth_token }
   let(:headers) do
     {
       'Content-Type' => Mime[:json].to_s,
       'Accept' => 'application/vnd.warehouse.v2',
-      'Authorization' => user.auth_token
+      'Authorization' => user.auth_token,
+      'access-token' => auth_data['access-token'],
+      'uid' => auth_data['uid'],
+      'client' => auth_data['client']
     }
   end
 
