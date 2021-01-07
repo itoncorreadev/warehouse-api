6ef62fcd977e66ffdec0cf420128591b6d1107b8
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-07 20:56:57 -0200

    Adding new I18n datetime format

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V2::TaskSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :done, :deadline, :user_id, :created_at, :updated_at, :short_description, :is_late
+  attributes :id, :title, :description, :done, :deadline, :user_id, :created_at, :updated_at, :short_description, :is_late, :deadline_to_br
 
   def short_description
     object.description[0..40] if object.description.present?
@@ -8,4 +8,9 @@ class Api::V2::TaskSerializer < ActiveModel::Serializer
   def is_late
     Time.current > object.deadline if object.deadline.present?
   end
+
+  def deadline_to_br
+    I18n.l(object.deadline, format: :datetime) if object.deadline.present?
+  end
+
 end
