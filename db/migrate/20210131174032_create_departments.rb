76da97a0bb7f2a8be988a16aef1150553e3c4423
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-31 15:43:01 -0200

    Improving department model

--- db/migrate/20210131174032_create_departments.rb
+++ db/migrate/20210131174032_create_departments.rb
@@ -2,7 +2,7 @@ class CreateDepartments < ActiveRecord::Migration[5.0]
   def change
     create_table :departments do |t|
       t.string :description
-      t.boolean :status
+      t.boolean :status, default: true
 
       t.timestamps
     end
