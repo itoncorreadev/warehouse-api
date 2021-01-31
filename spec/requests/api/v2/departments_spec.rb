357835210161a5fd4906f2e396e0506922aa8d59
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-31 16:45:13 -0200

    Improving index  action to the departments controller with tests

--- spec/requests/api/v2/departments_spec.rb
+++ spec/requests/api/v2/departments_spec.rb
@@ -32,5 +32,22 @@ RSpec.describe 'Department API' do
         expect(json_body[:data].count).to eq(5)
       end
     end
+
+    context 'when filter and sorting params is sent' do
+      let!(:department_1) { create(:department, description: 'Personal Department') }
+      let!(:department_2) { create(:department, description: 'Kitchen') }
+      let!(:department_3) { create(:department, description: 'Administrative') }
+      let!(:department_4) { create(:department, description: 'Information Technology') }
+
+      before do
+        get '/departments?q[description_cont]=en&q[s]=description+ASC', params: {}, headers: headers
+      end
+
+      it 'returns only the departments matching and in the correct order' do
+        returned_department_description = json_body[:data].map { |t| t[:attributes][:description] }
+
+        expect(returned_department_description).to eq([department_2.description, department_1.description])
+      end
+    end
   end
 end
