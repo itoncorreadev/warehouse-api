c37b8d9e2e811d3b224487f63ded901552f9cc8b
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-01-31 15:41:03 -0200

    Adding department model

---
+++ db/migrate/20210131174032_create_departments.rb
@@ -0,0 +1,10 @@
+class CreateDepartments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :departments do |t|
+      t.string :description
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
