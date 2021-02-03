8a42f31ef6fc2400f14f779e77a134037929536c
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-02-03 16:07:08 -0200

    Adding index  action to the requests controller with tests

--- app/controllers/api/v2/requests_controller.rb
+++ app/controllers/api/v2/requests_controller.rb
@@ -9,28 +9,6 @@ class Api::V2::RequestsController < Api::V2::BaseController
     render json: requests, status: 200
   end
 
-  def show
-    request = Request.find_by_id_and_product_id(params[:id], params[:product_id])
-
-    render json: request, status: 200
-  end
-
-  def create
-    product = Product.find(params[:product_id])
-    department = Department.first
-
-    request = Request.new(request_params)
-
-    request.product = product
-    request.department = department
-
-    if request.save
-      render json: request, status: 201
-    else
-      render json: { errors: request.errors }, status: 422
-    end
-  end
-
   private
 
   def request_params
