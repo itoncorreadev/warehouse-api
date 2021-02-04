9a1e2f95a8a849b077ca4481d7fcaa8b62b2b36a
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-02-04 12:51:10 -0200

    Adding seeds for Groups in Department

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Group.create(name: 'Diversos', status: true)
+Group.create([
+  {
+    name: 'Diversos',
+    status: true
+  },
+  {
+    name: 'Alimentos e Bebidas',
+    status: true
+  },
+  {
+    name: 'Limpeza e Higiene',
+    status: true
+  }
+])
+
+
+
+Department.create([
+  {
+    description: 'Administrativo',
+    status: true
+  },
+  {
+    description: 'Captação de Recursos',
+    status: true
+  },
+  {
+    description: 'Recursos Humanos',
+    status: true
+  },
+  {
+    description: 'Tecnologia da Informação',
+    status: true
+  },
+  {
+    description: 'Markting e Comunicação',
+    status: true
+  },
+  {
+    description: 'Educacional',
+    status: true
+  },
+  {
+    description: 'Social',
+    status: true
+  }
+])
