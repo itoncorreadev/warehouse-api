d0b9bb8519cda3f6fab62f1f5d0039b257869bf6
Cleiotn Corrêa <cleitoncorreas@gmail.com>
2021-03-02 14:04:30 -0300

    Improving serializer request

--- app/serializers/api/v2/request_serializer.rb
+++ app/serializers/api/v2/request_serializer.rb
@@ -1,7 +1,15 @@
 class Api::V2::RequestSerializer < ActiveModel::Serializer
-  attributes :date, :request_type, :description, :document_type, :document_code, :status, :supplier_id, :user_id, :department_id, :created_at, :updated_at, :date_to_br, :department, :supplier, :user
+  attributes :date, :request_type, :description, :document_type, :document_code, :status, :supplier_id, :user_id, :department_id, :created_at, :updated_at, :date_to_br, :department, :supplier, :user, :status_description
 
   def date_to_br
     I18n.l(object.date, format: :datetime) if object.date.present?
+  end
+
+  def status_description
+    if object.status
+      return "Concluída"
+    else
+      return "Pendente"
+    end
   end
 end
